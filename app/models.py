7ebe1ff6d839f0f221f167ab6390b367344bbd22
Brad Mitchell <BradleyMitchellis@yahoo.com>
2017-02-15 15:18:36 -0500

    Add PostgreSQL database

--- app/models.py
+++ app/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Statement(models.Model):
+    sentence = models.CharField(max_length=200)
+    blank = True
+    number = models.IntegerField()
+    def __self__(self):
+        return self.sentence
